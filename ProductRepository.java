4ea7046c8941823acb291e237f6c3b264c4e0c48
Sabaresh -K <sabaresh.k2025aids@sece.ac.in>
2026-09-18 12:57:01 +0530

    Delete ProductRepository.java

--- ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.productjpa.repository;
-
-import com.example.productjpa.model.Products;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Products, Long> {
-}
